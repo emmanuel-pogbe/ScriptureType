21ec799ec14ca1c1548fded07dbed47b69b922cf
Pogbe Emmanuel <mauyonpogbe@gmail.com>
2026-01-30 19:39:36 +0100

    Minor bug fix

--- static/script.js
+++ static/script.js
@@ -1061,7 +1061,7 @@ function isRegistered() { // Checks if user is registered correctly - soon to be
 }
 
 function displayUserFormCollection(bestScore,currentScore,testTypeString,selected) {
-  if (bestScore == currentScore && isRegistered()) {
+  if (bestScore == currentScore && !isRegistered()) {
     //Write code for updating best score as well
   userForm = document.getElementById("get-user-details");
   userForm.classList.remove("hidden");
